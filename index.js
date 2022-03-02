763de290e9a33a10831ee889962291d6e1957471
timble <timblemichael20@gmail.com>
2022-03-02 16:32:30 +1000

    completed with comments

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function introductionWithLanguage(name, language){
     return `Hi, my name is ${name} and I am learning to program in ${language}.`;
 }
 
-
+/* Takes in two paramters and uses where language is optional AND can be overidden with an argument */
 function introductionWithLanguageOptional(name, language="JavaScript"){
     return `Hi, my name is ${name} and I am learning to program in ${language}.`
 }
